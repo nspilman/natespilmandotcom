2259aa425c2c5887cd62f7896e5c89f3e7819bb3
Nate Spilman <nate.spilman@gmail.com>
2020-07-19 18:15:28 -0700

    formatting

--- src/components/homepage/postThumbnail.vue
+++ src/components/homepage/postThumbnail.vue
@@ -1,28 +1,26 @@
 <template>
-<article>
-   <h3>{{post.title}}</h3>
-          <h5>{{formatDate(post.date)}}</h5>
-          <p>{{post.description}}</p>
-          <ul class="actions">
-            <li>
-              <g-link :to="post.path" class="button">More</g-link>
-            </li>
-          </ul>
-</article>
+  <article>
+    <h3>{{post.title}}</h3>
+    <h5>{{formatDate(post.date)}}</h5>
+    <p>{{post.description}}</p>
+    <ul class="actions">
+      <li>
+        <g-link :to="post.path" class="button">More</g-link>
+      </li>
+    </ul>
+  </article>
 </template>
 
 <script>
 import formattedDateString from "../../utils/formattedDateString";
 
 export default {
-    props:[
-        'post'
-    ],
-      methods: {
+  props: ["post"],
+  methods: {
     formatDate(string) {
       return formattedDateString(string);
     }
-  },
+  }
 };
 </script>
 
