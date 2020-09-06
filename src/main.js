2744bf2d5bf4e1657d285d940dd6450b24ad6fa7
Nate Spilman <nate.spilman@gmail.com>
2020-09-05 22:42:04 -0700

    added main image as og:image for site

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import mainImage from "./assets/img/Jan52020-pro-photo.png";
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
+  head.meta.push({
+    key: 'og:image',
+    name: 'og:image',
+    content: mainImage,
+  })
   Vue.component('Layout', DefaultLayout)
 }
