e6348d7a1edac842efa0702b50a7beb39b7b5295
Nate Spilman <nate.spilman@gmail.com>
2020-12-14 18:35:05 -0800

    removing html tag from index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,4 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Nate Spilman</title>
-    </head>
-
     <body>
       <Layout>
         <section class="hero">
@@ -51,7 +43,6 @@
         </main>
       </Layout>
     </body>
-  </html>
 </template>
 
 <page-query>
