316bf3c23a2ed82375cb348b0b110f4c5501385f
Nate Spilman <nate.spilman@gmail.com>
2023-12-27 11:31:21 -0800

    configuring to allow Netlify deployment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withMDX = require("@next/mdx")();
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  experimental: {
+    appDir: true,
+  },
   // Optionally, add any other Next.js config below
 };
 
