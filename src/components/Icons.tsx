ef63a85703e94af3da658e225b9e64b832685100
Nate Spilman <nate.spilman@gmail.com>
2025-02-22 23:02:39 -0800

    adding bsky link

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,18 +4,23 @@ import Link from "next/link";
 export const Icons = () => (
   <div className="icon-container">
     <div>
-      <Link href="https://www.linkedin.com/in/natespilman/">
+      <Link href="https://www.linkedin.com/in/natespilman/" target="_blank" rel="noopener noreferrer">
         <img className="icon" src={"/img/linkedin@2x.png"} alt="linkedIn" />
       </Link>
     </div>
     <div>
-      <Link href="https://github.com/nspilman">
+      <Link href="https://github.com/nspilman" target="_blank" rel="noopener noreferrer">
         <img className="icon" src={"/img/github@2x.png"} alt="GitHub" />
       </Link>
     </div>
     <div>
-      <Link href="https://www.instagram.com/natespilman/">
+      <Link href="https://www.instagram.com/natespilman/" target="_blank" rel="noopener noreferrer">
         <img className="icon" src={"/img/instagram@2x.png"} alt="Instagram" />
+      </Link>
+    </div>
+    <div>
+      <Link href="https://bsky.app/profile/natespilman.com" target="_blank" rel="noopener noreferrer">
+        <img className="icon" src={"/img/bluesky@2x.png"} alt="Bluesky" />
       </Link>
     </div>
   </div>
